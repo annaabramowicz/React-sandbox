658e04e8eca06aebc44208c2520dfb46cbb8c1e0
anna abramowicz <annaabra1@gmail.com>
2020-05-06 14:19:17 +0200

    basic of function komponents and props

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
-import React from 'react';
+import React, { Children } from 'react';
 import './App.css';
+
+
+function Hello({firstName, lastName, children}) {
+  return(
+    <div className="App">
+      <p>Hello {firstName} {lastName}!!! !!!</p>
+      <p>{children}</p>
+    </div>
+  );
+}
+
+Hello.defaultProps = {
+  firstName: "Szkola",
+  // lastName : "Reacta"
+}
 
 function App() {
   return (
     <div className="App">
       test
         <h1>Test</h1>
+        <Hello firstName="Krzysiek" lastName="Nowak"/>
+        <Hello firstName="Krzysiek" />
+        <Hello>Tresc dziecka</Hello>
+        <Hello/>
     </div>
   );
 }
